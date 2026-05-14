4b202b59bd1d41c3b679d1a55867a578e0987851
Qustio <qustioof@gmail.com>
2026-05-14 16:24:47 +0300

    test: remove useless tests

--- test/q_overlay_test.cpp
+++ test/q_overlay_test.cpp
@@ -1,13 +1,13 @@
 #include <gtest/gtest.h>
 #include <layer.h>
 
-TEST(QOverlayTest, SanityCheck) {
-	EXPECT_EQ(1 + 1, 2);
-}
-
-TEST(QOverlayTest, AnotherCheck) {
-	EXPECT_TRUE(true);
-}
+// TEST(QOverlayTest, SanityCheck) {
+// 	EXPECT_EQ(1 + 1, 2);
+// }
+//
+// TEST(QOverlayTest, AnotherCheck) {
+// 	EXPECT_TRUE(true);
+// }
 
 TEST(QOverlayTest, link) {
 	EXPECT_EQ(aboba(1), 1);
