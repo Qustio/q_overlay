ba56d4ce7ac7654d0e0ba35fb31e9d66d07acc38
Qustio <qustioof@gmail.com>
2026-05-26 15:08:05 +0300

    refactor: split _WIN32 features

--- src/layer.cpp
+++ src/layer.cpp
@@ -1,12 +1,12 @@
+#include "src/state.cpp"
+
 #include <chrono>
 #include <cstdint>
 #include <imgui.h>
 #include <imgui_impl_vulkan.h>
 #include <imgui_impl_win32.h>
 #include <map>
-#include <mutex>
 #include <ratio>
-#include <shared_mutex>
 #include <spdlog/fmt/fmt.h>
 #include <vulkan/utility/vk_dispatch_table.h>
 #include <vulkan/vk_layer.h>
@@ -14,26 +14,23 @@
 #include <vulkan/vulkan.hpp>
 #include <vulkan/vulkan_core.h>
 
-#include "src/global_state.cpp"
-
 #undef EXPORT
 #ifdef _WIN32
 	#define EXPORT __declspec(dllexport)
+extern IMGUI_IMPL_API auto ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) -> LRESULT;
 #else
 	#define EXPORT
 #endif
 
-extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
-
 namespace {
-	globals state;
+	state::state s;
 	// https://github.com/ocornut/imgui/issues/4854#issuecomment-1783950012
 	void init_imgui_vulkan(
 		vk::Instance instance,
 		vk::Device device,
 		uint32_t api_version
 	) {
-		state.l.info(
+		s.l.info(
 			"Vulkan API version: {}.{}.{}",
 			VK_API_VERSION_MAJOR(api_version),
 			VK_API_VERSION_MINOR(api_version),
@@ -50,14 +47,14 @@ namespace {
 		auto res = ImGui_ImplVulkan_LoadFunctions(
 			api_version,
 			[](const char *name, void *user_data) -> PFN_vkVoidFunction {
-				state.l.trace(name);
+				s.l.trace(name);
 
 				auto *data = reinterpret_cast<loader_data *>(user_data);
-				PFN_vkGetDeviceProcAddr device_func = state.device_dispatch.read()->at(get_key(data->device)).vkGetDeviceProcAddr;
-				PFN_vkGetInstanceProcAddr instance_func = state.instance_dispatch.read()->at(get_key(data->instance)).vkGetInstanceProcAddr;
+				PFN_vkGetDeviceProcAddr device_func = s.device_dispatch.read()->at(get_key(data->device)).vkGetDeviceProcAddr;
+				PFN_vkGetInstanceProcAddr instance_func = s.instance_dispatch.read()->at(get_key(data->instance)).vkGetInstanceProcAddr;
 				auto device_addr = device_func(data->device, name);
 				if (device_addr) {
-					state.l.trace("device");
+					s.l.trace("device");
 					return device_addr;
 				}
 				auto name_view = std::string_view{name};
@@ -69,16 +66,16 @@ namespace {
 				}
 				auto instance_addr = instance_func(data->instance, name);
 				if (instance_addr) {
-					state.l.trace("instance");
+					s.l.trace("instance");
 					return instance_addr;
 				}
-				state.l.trace("unknown function");
-				state.l.flush();
+				s.l.trace("unknown function");
+				s.l.flush();
 				return nullptr;
 			},
 			&loaders
 		);
-		state.l.info("ImGui_ImplVulkan_LoadFunctions returned: {}", res);
+		s.l.info("ImGui_ImplVulkan_LoadFunctions returned: {}", res);
 	}
 
 	VKAPI_ATTR auto VKAPI_CALL
@@ -87,7 +84,7 @@ namespace {
 		const VkAllocationCallbacks *pAllocator,
 		VkInstance *pInstance
 	) -> VkResult {
-		state.l.trace(__func__);
+		s.l.trace(__func__);
 
 		auto *layerCreateInfo =
 			reinterpret_cast<VkLayerInstanceCreateInfo *>(
@@ -125,14 +122,14 @@ namespace {
 		vk::detail::DispatchLoaderDynamic dispatchTable{*pInstance, gipa};
 
 		vk::Instance instance{*pInstance};
-		state.instance_dispatch.mutate([&](auto &i_dld) {
+		s.instance_dispatch.mutate([&](auto &i_dld) {
 			i_dld[get_key(instance)] = dispatchTable;
 		});
-		state.instance_map.mutate([&](auto &i_map) {
+		s.instance_map.mutate([&](auto &i_map) {
 			i_map[get_key(instance)] = instance;
 		});
 
-		state.update_imgui_init_info([&](auto &info) -> void {
+		s.update_imgui_init_info([&](auto &info) -> void {
 			info.Instance = *pInstance;
 			info.UseDynamicRendering = true;
 			info.ApiVersion = pCreateInfo->pApplicationInfo->apiVersion;
@@ -149,7 +146,7 @@ namespace {
 		const VkAllocationCallbacks *pAllocator,
 		VkDevice *pDevice
 	) -> VkResult {
-		state.l.trace(__func__);
+		s.l.trace(__func__);
 
 		auto *layerCreateInfo =
 			reinterpret_cast<VkLayerDeviceCreateInfo *>(
@@ -188,22 +185,22 @@ namespace {
 			return result;
 		}
 
-		vk::Instance instance = state.instance_map.read()->at(get_key(physicalDevice));
+		vk::Instance instance = s.instance_map.read()->at(get_key(physicalDevice));
 		vk::Device device{*pDevice};
-		vk::detail::DispatchLoaderDynamic copy_dld = state.instance_dispatch.read()->at(get_key(physicalDevice));
+		vk::detail::DispatchLoaderDynamic copy_dld = s.instance_dispatch.read()->at(get_key(physicalDevice));
 		copy_dld.vkGetDeviceProcAddr = gdpa;
 		copy_dld.init(device);
-		state.device_dispatch.mutate([&](auto &d_dld) {
+		s.device_dispatch.mutate([&](auto &d_dld) {
 			d_dld[get_key(device)] = copy_dld;
 		});
 
 		uint32_t api;
-		state.update_imgui_init_info([&](ImGui_ImplVulkan_InitInfo &info) -> void {
+		s.update_imgui_init_info([&](ImGui_ImplVulkan_InitInfo &info) -> void {
 			api = info.ApiVersion;
 			info.PhysicalDevice = physicalDevice;
 			info.Device = *pDevice;
-			state.l.info("PhysicalDevice");
-			state.l.info("Device");
+			s.l.info("PhysicalDevice");
+			s.l.info("Device");
 		});
 		init_imgui_vulkan(instance, device, api);
 
@@ -212,17 +209,15 @@ namespace {
 
 #ifdef VK_USE_PLATFORM_WIN32_KHR
 
-	static LRESULT CALLBACK Q_HookedWndProc(
+	auto CALLBACK Q_HookedWndProc(
 		HWND hwnd,
 		UINT msg,
 		WPARAM wParam,
 		LPARAM lParam
-	) {
-		// Let ImGui see the message first.
+	) -> LRESULT {
 		if (ImGui::GetCurrentContext() != nullptr) {
 			ImGui_ImplWin32_WndProcHandler(hwnd, msg, wParam, lParam);
 
-			// If ImGui currently wants the input, swallow it.
 			ImGuiIO &io = ImGui::GetIO();
 			switch (msg) {
 				case WM_LBUTTONDOWN:
@@ -250,8 +245,7 @@ namespace {
 			}
 		}
 
-		// Forward to whatever the app originally installed.
-		return CallWindowProcW(state.win32_hook.original_wndproc, hwnd, msg, wParam, lParam);
+		return CallWindowProcW(s.win32_hook.original_wndproc, hwnd, msg, wParam, lParam);
 	}
 
 	VKAPI_ATTR auto VKAPI_CALL
@@ -261,19 +255,19 @@ namespace {
 		const VkAllocationCallbacks *pAllocator,
 		VkSurfaceKHR *pSurface
 	) -> VkResult {
-		state.l.trace(__func__);
+		s.l.trace(__func__);
 
-		PFN_vkCreateWin32SurfaceKHR func = state.instance_dispatch.read()->at(get_key(instance)).vkCreateWin32SurfaceKHR;
+		PFN_vkCreateWin32SurfaceKHR func = s.instance_dispatch.read()->at(get_key(instance)).vkCreateWin32SurfaceKHR;
 
 		HWND hwnd = pCreateInfo->hwnd;
-		if (state.win32_hook.hwnd != hwnd) {
+		if (s.win32_hook.hwnd != hwnd) {
 			ImGui_ImplWin32_Init(pCreateInfo->hwnd);
-			state.win32_hook.hwnd = hwnd;
-			state.win32_hook.original_wndproc = reinterpret_cast<WNDPROC>(
+			s.win32_hook.hwnd = hwnd;
+			s.win32_hook.original_wndproc = reinterpret_cast<WNDPROC>(
 				SetWindowLongPtrW(hwnd, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(&Q_HookedWndProc))
 			);
 
-			state.l.trace("win32 wndproc subclassed for hwnd {}", static_cast<void *>(hwnd));
+			s.l.trace("win32 wndproc subclassed for hwnd {}", static_cast<void *>(hwnd));
 		}
 
 		return func(instance, pCreateInfo, pAllocator, pSurface);
@@ -303,20 +297,20 @@ namespace {
 		uint32_t queueIndex,
 		VkQueue *pQueue
 	) {
-		state.l.debug(__func__);
+		s.l.debug(__func__);
 
-		PFN_vkGetDeviceQueue func = state.device_dispatch.read()->at(get_key(device)).vkGetDeviceQueue;
+		PFN_vkGetDeviceQueue func = s.device_dispatch.read()->at(get_key(device)).vkGetDeviceQueue;
 
 		func(device, queueFamilyIndex, queueIndex, pQueue);
-		state.l.debug("Family: {} Queue: {}", queueFamilyIndex, queueIndex);
+		s.l.debug("Family: {} Queue: {}", queueFamilyIndex, queueIndex);
 		if (queueFamilyIndex != 0 || queueIndex != 0) {
 			return;
 		}
-		state.update_imgui_init_info([&](ImGui_ImplVulkan_InitInfo &info) -> void {
+		s.update_imgui_init_info([&](ImGui_ImplVulkan_InitInfo &info) -> void {
 			info.QueueFamily = queueFamilyIndex;
 			info.Queue = *pQueue;
-			state.l.info("Queue");
-			state.l.info("QueueFamily");
+			s.l.info("Queue");
+			s.l.info("QueueFamily");
 		});
 	}
 
@@ -326,20 +320,20 @@ namespace {
 		const VkDeviceQueueInfo2 *pQueueInfo,
 		VkQueue *pQueue
 	) {
-		state.l.debug(__func__);
+		s.l.debug(__func__);
 
-		PFN_vkGetDeviceQueue2 func = state.device_dispatch.read()->at(get_key(device)).vkGetDeviceQueue2;
+		PFN_vkGetDeviceQueue2 func = s.device_dispatch.read()->at(get_key(device)).vkGetDeviceQueue2;
 
 		func(device, pQueueInfo, pQueue);
-		state.l.debug("Family: {} Queue: {}", pQueueInfo->queueFamilyIndex, pQueueInfo->queueIndex);
+		s.l.debug("Family: {} Queue: {}", pQueueInfo->queueFamilyIndex, pQueueInfo->queueIndex);
 		if (pQueueInfo->queueFamilyIndex != 0 || pQueueInfo->queueIndex != 0) {
 			return;
 		}
-		state.update_imgui_init_info([&](ImGui_ImplVulkan_InitInfo &info) -> void {
+		s.update_imgui_init_info([&](ImGui_ImplVulkan_InitInfo &info) -> void {
 			info.QueueFamily = pQueueInfo->queueFamilyIndex;
 			info.Queue = *pQueue;
-			state.l.info("Queue");
-			state.l.info("QueueFamily");
+			s.l.info("Queue");
+			s.l.info("QueueFamily");
 		});
 	}
 
@@ -350,9 +344,9 @@ namespace {
 		const VkAllocationCallbacks *pAllocator,
 		VkRenderPass *pRenderPass
 	) -> VkResult {
-		state.l.debug(__func__);
+		s.l.debug(__func__);
 
-		PFN_vkCreateRenderPass func = state.device_dispatch.read()->at(get_key(device)).vkCreateRenderPass;
+		PFN_vkCreateRenderPass func = s.device_dispatch.read()->at(get_key(device)).vkCreateRenderPass;
 
 		auto result = func(device, pCreateInfo, pAllocator, pRenderPass);
 
@@ -367,9 +361,9 @@ namespace {
 		const VkAllocationCallbacks *pAllocator,
 		VkSwapchainKHR *pSwapchain
 	) -> VkResult {
-		state.l.debug(__func__);
+		s.l.debug(__func__);
 
-		PFN_vkCreateSwapchainKHR func = state.device_dispatch.read()->at(get_key(device)).vkCreateSwapchainKHR;
+		PFN_vkCreateSwapchainKHR func = s.device_dispatch.read()->at(get_key(device)).vkCreateSwapchainKHR;
 
 		auto result = func(device, pCreateInfo, pAllocator, pSwapchain);
 		ImGui::GetIO().DisplaySize = ImVec2(
@@ -379,23 +373,23 @@ namespace {
 		if (result != VK_SUCCESS) {
 			return result;
 		}
-		state.init_swapchain_data(
+		s.init_swapchain_data(
 			device,
 			*pSwapchain,
 			*pCreateInfo
 		);
-		state.update_imgui_init_info([&](ImGui_ImplVulkan_InitInfo &info) -> void {
-			state.swapchain_color_format = vk::Format{pCreateInfo->imageFormat};
+		s.update_imgui_init_info([&](ImGui_ImplVulkan_InitInfo &info) -> void {
+			s.swapchain_color_format = vk::Format{pCreateInfo->imageFormat};
 			auto pci = vk::PipelineRenderingCreateInfo(
-				0,							   // viewMask
-				1,							   // colorAttachmentCount
-				&state.swapchain_color_format, // pColorAttachmentFormats
-				vk::Format::eUndefined,		   // depthAttachmentFormat
-				vk::Format::eUndefined		   // stencilAttachmentFormat
+				0,						   // viewMask
+				1,						   // colorAttachmentCount
+				&s.swapchain_color_format, // pColorAttachmentFormats
+				vk::Format::eUndefined,	   // depthAttachmentFormat
+				vk::Format::eUndefined	   // stencilAttachmentFormat
 			);
 			info.MinImageCount = pCreateInfo->minImageCount;
 			info.PipelineInfoMain.PipelineRenderingCreateInfo = pci;
-			state.l.info("MinImageCount: {}", pCreateInfo->minImageCount);
+			s.l.info("MinImageCount: {}", pCreateInfo->minImageCount);
 		});
 		return result;
 	}
@@ -406,11 +400,11 @@ namespace {
 		VkSwapchainKHR swapchain,
 		const VkAllocationCallbacks *pAllocator
 	) {
-		state.l.debug(__func__);
+		s.l.debug(__func__);
 
-		PFN_vkDestroySwapchainKHR func = state.device_dispatch.read()->at(get_key(device)).vkDestroySwapchainKHR;
+		PFN_vkDestroySwapchainKHR func = s.device_dispatch.read()->at(get_key(device)).vkDestroySwapchainKHR;
 
-		state.remove_swapchain_data(swapchain);
+		s.remove_swapchain_data(swapchain);
 		func(device, swapchain, pAllocator);
 	}
 
@@ -419,13 +413,13 @@ namespace {
 		VkDevice device,
 		const VkAllocationCallbacks *pAllocator
 	) {
-		state.l.trace(__func__);
+		s.l.trace(__func__);
 
-		state.shutdown_imgui();
+		s.shutdown_imgui();
 
-		PFN_vkDestroyDevice func = state.device_dispatch.read()->at(get_key(device)).vkDestroyDevice;
+		PFN_vkDestroyDevice func = s.device_dispatch.read()->at(get_key(device)).vkDestroyDevice;
 
-		state.device_dispatch.mutate([&](auto &d_dld) {
+		s.device_dispatch.mutate([&](auto &d_dld) {
 			d_dld.erase(get_key(device));
 		});
 		func(device, pAllocator);
@@ -438,9 +432,9 @@ namespace {
 		const VkSubmitInfo *pSubmits,
 		VkFence fence
 	) -> VkResult {
-		state.l.trace(__func__);
+		s.l.trace(__func__);
 
-		PFN_vkQueueSubmit func = state.device_dispatch.read()->at(get_key(queue)).vkQueueSubmit;
+		PFN_vkQueueSubmit func = s.device_dispatch.read()->at(get_key(queue)).vkQueueSubmit;
 
 		return func(queue, submitCount, pSubmits, fence);
 	}
@@ -450,15 +444,15 @@ namespace {
 		VkQueue queue,
 		const VkPresentInfoKHR *pPresentInfo
 	) -> VkResult {
-		state.l.trace(__func__);
+		s.l.trace(__func__);
 
-		PFN_vkQueuePresentKHR func = state.device_dispatch.read()->at(get_key(queue)).vkQueuePresentKHR;
+		PFN_vkQueuePresentKHR func = s.device_dispatch.read()->at(get_key(queue)).vkQueuePresentKHR;
 
 		auto result = func(queue, pPresentInfo);
 		auto now = std::chrono::high_resolution_clock::now();
-		auto elapsed = std::chrono::duration<double, std::milli>(now - state.last_frame);
-		state.frametime = elapsed.count();
-		state.last_frame = now;
+		auto elapsed = std::chrono::duration<double, std::milli>(now - s.last_frame);
+		s.frametime = elapsed.count();
+		s.last_frame = now;
 
 		return result;
 	}
@@ -467,9 +461,9 @@ namespace {
 	Q_CmdEndRenderPass(
 		VkCommandBuffer commandBuffer
 	) {
-		state.l.trace(__func__);
+		s.l.trace(__func__);
 
-		PFN_vkCmdEndRenderPass func = state.device_dispatch.read()->at(get_key(commandBuffer)).vkCmdEndRenderPass;
+		PFN_vkCmdEndRenderPass func = s.device_dispatch.read()->at(get_key(commandBuffer)).vkCmdEndRenderPass;
 
 		// auto *data = state.imgui();
 		// if (data != nullptr) {
@@ -483,9 +477,9 @@ namespace {
 		VkCommandBuffer commandBuffer,
 		const VkSubpassEndInfo *pSubpassEndInfo
 	) {
-		state.l.trace(__func__);
+		s.l.trace(__func__);
 
-		PFN_vkCmdEndRenderPass2 func = state.device_dispatch.read()->at(get_key(commandBuffer)).vkCmdEndRenderPass2;
+		PFN_vkCmdEndRenderPass2 func = s.device_dispatch.read()->at(get_key(commandBuffer)).vkCmdEndRenderPass2;
 
 		// auto *data = state.imgui();
 		// if (data != nullptr) {
@@ -498,11 +492,11 @@ namespace {
 	Q_CmdEndRendering(
 		VkCommandBuffer commandBuffer
 	) {
-		state.l.trace(__func__);
+		s.l.trace(__func__);
 
-		PFN_vkCmdEndRendering func = state.device_dispatch.read()->at(get_key(commandBuffer)).vkCmdEndRendering;
+		PFN_vkCmdEndRendering func = s.device_dispatch.read()->at(get_key(commandBuffer)).vkCmdEndRendering;
 
-		auto *data = state.imgui();
+		auto *data = s.imgui();
 		if (data != nullptr) {
 			ImGui_ImplVulkan_RenderDrawData(data, commandBuffer);
 		}
@@ -553,7 +547,7 @@ extern "C" {
 			return iter->second;
 		}
 		{
-			PFN_vkGetDeviceProcAddr func = state.device_dispatch.read()->at(get_key(device)).vkGetDeviceProcAddr;
+			PFN_vkGetDeviceProcAddr func = s.device_dispatch.read()->at(get_key(device)).vkGetDeviceProcAddr;
 			return func(device, pName);
 		}
 	}
@@ -569,7 +563,7 @@ extern "C" {
 			return iter->second;
 		}
 		{
-			PFN_vkGetInstanceProcAddr func = state.instance_dispatch.read()->at(get_key(instance)).vkGetInstanceProcAddr;
+			PFN_vkGetInstanceProcAddr func = s.instance_dispatch.read()->at(get_key(instance)).vkGetInstanceProcAddr;
 			return func(instance, pName);
 		}
 	}
